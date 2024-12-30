2d3e8cd5a8d8da60c3a191b51de3758db48db799
Natsu JING <118078044+matsu233511@users.noreply.github.com>
2024-12-30 17:38:46 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -5,7 +5,7 @@ export default siteConfig({
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
-  rootNotionSpaceId: null,
+  rootNotionSpaceId: '16ce064ab27c80dca088f466294b4613',
 
   // basic site info (required)
   name: 'Name',
